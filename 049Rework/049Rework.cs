ba2d694246732f7f1d60cda51554c7419b81652f
lucasboss45 <l4f4go@gmail.com>
2019-05-31 15:50:11 +0200

    Initial commit

---
+++ 049Rework/049Rework.cs
@@ -0,0 +1,44 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.Lang;
+using Smod2.Piping;
+
+namespace PlagueRework
+{
+	[PluginDetails(
+		author = "lucasboss45",
+		name = "049 Rework",
+		description = "A rework for 049",
+		id = "lucas.plaguerework",
+		version = "1.0",
+		SmodMajor = 3,
+		SmodMinor = 3,
+		SmodRevision = 1
+		)]
+	public class PlagueRework : Plugin
+	{
+		public override void OnDisable()
+		{
+			this.Info("049 Rework disabled");
+		}
+
+		public override void OnEnable()
+		{
+			this.Info("049 Rework enabled");
+		}
+		
+		public override void Register()
+		{
+			
+			this.AddEventHandlers(new RoundEventHandler(this));
+            this.AddConfig(new ConfigSetting("049_maximumhptoheal", 1000, true, "The limit of HP 049 can be healed to."));
+            this.AddConfig(new ConfigSetting("049_maximumhealpersecond", 5, true, "Maximum HP per second 049 can be healed"));
+            this.AddConfig(new ConfigSetting("049_healradius", 5, true, "Radius where SCP-049 heals SCPs."));
+            this.AddConfig(new ConfigSetting("049_hptohealotherscps", 1, true, "The amount of HP to give to other SCPs when near SCP-049"));
+		}
+	}
+}
---
+++ 049Rework/RoundEventHandler.cs
@@ -0,0 +1,119 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+using MEC;
+using UnityEngine;
+
+namespace PlagueRework
+{
+	class RoundEventHandler : IEventHandlerRoundStart, IEventHandlerRoundEnd, IEventHandlerSetRole
+	{
+		private readonly PlagueRework plugin;
+
+		public RoundEventHandler(PlagueRework plugin) => this.plugin = plugin;
+
+        private bool RoundInProgress = false;
+        private int zombies = 0;
+        public Player plaguecache = null;
+
+        private IEnumerator<float> Heal()
+        {
+            yield return Timing.WaitForSeconds(2f);
+            while(RoundInProgress == true)
+            {
+                yield return Timing.WaitForSeconds(1f);
+                if(plugin.GetConfigInt("049_maximumhealpersecond") > zombies && plugin.GetConfigInt("049_maximumhealpersecond") != 0)
+                {
+                    if(plaguecache.GetHealth() < plugin.GetConfigInt("049_maximumhptoheal")) {
+                        plaguecache.AddHealth(zombies);
+                    }
+                } else if (plugin.GetConfigInt("049_maximumhealpersecond") < zombies && plugin.GetConfigInt("049_maximumhealpersecond") != 0) {
+                    plaguecache.AddHealth(plugin.GetConfigInt("049_maximumhealpersecond"));
+                }
+                foreach(Player player in plugin.Server.GetPlayers())
+                {
+                    if(Vector.Distance(player.GetPosition(), plaguecache.GetPosition()) <= plugin.GetConfigInt("049_healradius"))
+                    {
+                        if(player.TeamRole.Role == Role.SCP_173 && player.GetHealth() < 3200)
+                        {
+                            player.AddHealth(plugin.GetConfigInt("049_hptohealotherscps"));
+                        } else if(player.TeamRole.Role == Role.SCP_106 && player.GetHealth() < 650)
+                        {
+                            player.AddHealth(plugin.GetConfigInt("049_hptohealotherscps"));
+                        } else if(player.TeamRole.Role == Role.SCP_096 && player.GetHealth() < 2000)
+                        {
+                            player.AddHealth(plugin.GetConfigInt("049_hptohealotherscps"));
+                        } else if(player.TeamRole.Role == Role.SCP_939_53 && player.GetHealth() < 2200 | player.TeamRole.Role == Role.SCP_939_89 && player.GetHealth() < 2200)
+                        {
+                            player.AddHealth(plugin.GetConfigInt("049_hptohealotherscps"));
+                        } else if(player.TeamRole.Role == Role.SCP_049_2 && player.GetHealth() < 400)
+                        {
+                            player.AddHealth(plugin.GetConfigInt("049_hptohealotherscps"));
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerator<float> ZombieTimer()
+        {
+            yield return Timing.WaitForSeconds(2f);
+            while(RoundInProgress == true)
+            {
+                yield return Timing.WaitForSeconds(5f);
+                CheckZombies();
+            }
+        }
+
+        private IEnumerator<float> Plaguecache()
+        {
+            yield return Timing.WaitForSeconds(5f);
+            foreach (Player player in plugin.Server.GetPlayers())
+            {
+                if(player.TeamRole.Role == Role.SCP_049)
+                {
+                    plaguecache = player;
+                }
+            }
+            Timing.RunCoroutine(Heal());
+            Timing.RunCoroutine(ZombieTimer());
+        }
+
+		public void OnRoundEnd(RoundEndEvent ev)
+		{
+            RoundInProgress = false;
+		}
+
+		public void OnRoundStart(RoundStartEvent ev)
+		{
+            RoundInProgress = true;
+            zombies = 0;
+            plaguecache = null;
+            Timing.RunCoroutine(Plaguecache());
+		}
+
+        public void OnSetRole(PlayerSetRoleEvent ev)
+        {
+            CheckZombies();
+            if(ev.Role == Role.SCP_049)
+            {
+                plaguecache = ev.Player;
+            }
+        }
+
+        private void CheckZombies()
+        {
+            int zombiecount = 0;
+            foreach (Player player in plugin.Server.GetPlayers()) // Updating zombie count
+            {
+                if (player.TeamRole.Role == Role.SCP_049_2)
+                {
+                    zombiecount += 1;
+                }
+            }
+            zombies = zombiecount;
+        }
+    }
+}
